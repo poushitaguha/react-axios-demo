30cc33d18953d5eef05231b6118f5927f3f8abce
poushitaguha <pguha.kolkata@gmail.com>
2019-11-20 13:13:36 -0500

    Create Routing of components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Create from './components/Create'
+import Index from './components/Index'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
-    <div class="container">
-      React Axios Tutorial
-    </div>
+    <Router>
+      <div class="container">
+        <h2>Welcome to React Express Tutorial</h2>
+        <ul>
+          <li><Link to={'/create'}>Create</Link></li>
+          <li><Link to={'/index'}>List</Link></li>
+        </ul>
+        <hr />
+        <Switch>
+          <Route exact path='/create' component={Create} />
+          <Route path='/index' component={Index} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
